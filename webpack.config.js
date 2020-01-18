e3ea017ea5aa77ac25e288459a39002df4477d2c
Daniel Gowing <danielgowing@Daniels-MacBook-Pro.local>
2020-01-18 16:39:14 +0000

    setup bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,8 @@ Encore
     // uncomment if you use Sass/SCSS files
     .enableSassLoader()
 
+    .enableReactPreset()
+
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 ;
