dfba4c308c9e71a22a74335db21c5bd62d68fd90
Daniel Gowing <danielgowing@Daniels-MacBook-Pro.local>
2020-01-26 01:14:26 +0000

    mobile design

--- public_html/build/scripts.js
+++ public_html/build/scripts.js
@@ -1,1 +1,16 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["scripts"],{kpGR:function(e,n){console.log("testing"),document.querySelector(".test").innerHTML="correct"}},[["kpGR","runtime"]]]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["scripts"],{
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log("testing");
+document.querySelector('.test').innerHTML = "correct";
+
+/***/ })
+
+},[["./src/js/main.js","runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvanMvbWFpbi5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIiwiZG9jdW1lbnQiLCJxdWVyeVNlbGVjdG9yIiwiaW5uZXJIVE1MIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQUEsT0FBTyxDQUFDQyxHQUFSLENBQVksU0FBWjtBQUVBQyxRQUFRLENBQUNDLGFBQVQsQ0FBdUIsT0FBdkIsRUFBZ0NDLFNBQWhDLEdBQTRDLFNBQTVDLEMiLCJmaWxlIjoic2NyaXB0cy5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwidGVzdGluZ1wiKTtcblxuZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLnRlc3QnKS5pbm5lckhUTUwgPSBcImNvcnJlY3RcIjsiXSwic291cmNlUm9vdCI6IiJ9
